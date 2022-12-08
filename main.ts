9d40194175a3009fb33c707a73b8b08541045daa
418098 <115142584+418098@users.noreply.github.com>
2022-12-08 16:47:32 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,31 @@
+let x = 0
+let y = 0
+let list: number[] = []
+let n = 0
+let a = 0
+function 燈數 (num: number) {
+    if (num % 5 == 0) {
+        x = 4
+        y = Math.floor(num / 5) - 1
+    } else {
+        x = Math.floor(num / 5) - 1
+        y = 4
+    }
+    led.plot(x, y)
+}
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    list = []
+    for (let index = 0; index < 25; index++) {
+        list.push(0)
+    }
+    n = 0
+    while (n < 3) {
+        a = randint(0, 24)
+        if (list[a]) {
+            list[a] = 1
+            n += 1
+            燈數(a + 1)
+        }
+    }
+})
